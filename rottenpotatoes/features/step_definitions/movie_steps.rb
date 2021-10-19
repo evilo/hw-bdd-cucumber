66999dabf9bdb54aab43b7ad9929997a7c454d00
Olivia Zhang <xz2997@columbia.edu>
2021-10-19 04:02:20 +0000

    done

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,23 +2,20 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+      Movie.create movie
   end
-  fail "Unimplemented"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
-  Movie.count.should be n_seeds.to_i
+    Movie.count.should be n_seeds.to_i
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+    contents = page.body()
+    expect(contents.index(e1)).to be < contents.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -26,13 +23,18 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+    if uncheck
+        rating_list.split(/, /, -1).each{|rating| uncheck("ratings_" + rating)}
+    else
+        rating_list.split(/, /, -1).each{|rating| check("ratings_" + rating)}
+    end
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+#   fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+    c = Movie.all.count + 1
+    expect(page.all('table#movies tr').count).to eq (c)
 end
